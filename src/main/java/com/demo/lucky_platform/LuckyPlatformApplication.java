4b80633933184f7a963bc1db89bdbaf0a69d9bad
PJH447 <bopi44777@gmail.com>
2024-10-14 09:07:36 +0900

    init

---
+++ src/main/java/com/demo/lucky_platform/LuckyPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.demo.lucky_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckyPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuckyPlatformApplication.class, args);
+	}
+
+}
