c8368404d69c91c64ef8e475c26abec518e486a3
PJH447 <bopi44777@gmail.com>
2024-10-14 16:08:33 +0900

    WebConfig - BCryptPasswordEncoder

---
+++ src/main/java/com/demo/lucky_platform/config/other/WebConfig.java
@@ -0,0 +1,15 @@
+package com.demo.lucky_platform.config.other;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
