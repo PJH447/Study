f50689c94204b62015c33f4521744b9ffc08f1ee
PJH447 <bopi44777@gmail.com>
2024-10-15 13:30:53 +0900

    iamport setting

--- src/main/java/com/demo/lucky_platform/config/other/WebConfig.java
+++ src/main/java/com/demo/lucky_platform/config/other/WebConfig.java
@@ -1,5 +1,7 @@
 package com.demo.lucky_platform.config.other;
 
+import com.siot.IamportRestClient.IamportClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -7,9 +9,19 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 public class WebConfig {
 
+    @Value("${app.iamport.api-key}")
+    String iamportApiKey;
+
+    @Value("${app.iamport.api-secret}")
+    String iamportApiSecret;
+
     @Bean
     BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public IamportClient iamportClient() {
+        return new IamportClient(iamportApiKey, iamportApiSecret);
+    }
 }
