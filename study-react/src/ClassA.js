1c154d4b62f5ad744576784facd039863f1b3f9b
PJH447 <bopi44777@gmail.com>
2024-10-22 17:11:52 +0900

    react 로그인 관련 버튼 axios

--- study-react/src/ClassA.js
+++ study-react/src/ClassA.js
@@ -9,20 +9,84 @@ function ClassA() {
         setMessage('new Message');
     };
 
-    const handleClick = () => {
-        console.log(message);
-        axios.get('http://127.0.0.1:9003/api/test/v1', {
+    const login = () => {
+        axios.post('http://127.0.0.1:9003/api/auth/v1/login', {
+            "email": "test@naver.com",
+            "password": "password"
+        },{
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            withCredentials: true
         }).then(response => {
-            console.log('then')
+
+            const authorizationHeader = response.headers.authorization;
+            axios.defaults.headers.common[
+                'Authorization'
+                ] = `Bearer ${authorizationHeader}`;
+
         }).catch(error => {
-            console.log('error')
             console.log(error);
             return false;
         });
     };
 
-    return <><div onClick={handleClick}>is ClassA</div>
-    <div onClick={event}>is ClassA2</div></>;
+    const reissue = () => {
+        console.log(message);
+        axios.post('http://127.0.0.1:9003/api/auth/v1/reissue', {
+        },{
+            headers:{
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            withCredentials: true
+        }).then(response => {
+            console.log(response)
+        }).catch(error => {
+            console.log(error);
+            return false;
+        });
+    };
+
+    const logout = () => {
+        console.log(message);
+        axios.post('http://127.0.0.1:9003/api/auth/v1/logout', {
+        },{
+            headers:{
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }).then(response => {
+            console.log(response)
+
+            axios.defaults.headers.common[
+                'Authorization'
+                ] = `Bearer `;
+
+        }).catch(error => {
+            console.log(error);
+            return false;
+        });
+    };
+
+    const getTest = () => {
+        axios.get('http://127.0.0.1:9003/api/test/v2',{
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        }).then(response => {
+            console.log(response)
+
+        }).catch(error => {
+            console.log(error);
+            return false;
+        });
+    };
+
+    return <>
+        <div onClick={login}>is login</div>
+        <div onClick={reissue}>is reissue</div>
+        <div onClick={logout}>is logout</div>
+        <div onClick={getTest}>is ClassA2</div>
+    </>;
 }
 
 export default ClassA;
