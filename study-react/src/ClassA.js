27ed722c3fc0ee5fd2d9dd08394c1f966778cf00
PJH447 <bopi44777@gmail.com>
2024-10-28 10:36:38 +0900

    remove redux

--- study-react/src/ClassA.js
+++ study-react/src/ClassA.js
@@ -1,7 +1,5 @@
 import React, {useEffect, useRef, useState} from 'react';
 import axios from "axios";
-import {useDispatch} from 'react-redux';
-import {setAccessToken, delAccessToken} from './Reducer';
 
 function login(inputName, inputPassword) {
 
