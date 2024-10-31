4ee60f80e06f46fd9d49b4e1a205b60deb519641
PJH447 <bopi44777@gmail.com>
2024-10-31 12:55:58 +0900

    -

--- study-react/src/component/SignUp.jsx
+++ study-react/src/component/SignUp.jsx
@@ -1,43 +1,65 @@
 import React, {useEffect, useRef, useState} from "react";
 import './signUp.css';
 import axios from "axios";
-
-function useInputLengthSlicer(inputRef, maxLength) {
-    useEffect(() => {
-        const input = inputRef.current;
-        if (input) {
-            input.addEventListener('input', (event) => {
-                if (input.value.length > maxLength) {
-                    input.value = input.value.slice(0, maxLength);
-                }
-            });
-        }
-    }, [inputRef]);
-}
-
+import {useDebounce, useInputLengthSlicer} from "../util";
 
 function SignUp() {
 
-    const inputEmail = useRef(null);
-    const inputPassword = useRef(null)
-    const inputConfirmPassword = useRef(null)
-    const inputNickname = useRef(null);
-    const inputName = useRef(null);
-    const inputPhone1 = useRef(null);
-    const inputPhone2 = useRef(null);
-    const inputPhone3 = useRef(null);
+    const [password, setPassword] = useState();
+    const handleChangePassword = (e) => {
+        setPassword(e.target.value);
+    };
 
-    useInputLengthSlicer(inputPhone1, 3);
-    useInputLengthSlicer(inputPhone2, 4);
-    useInputLengthSlicer(inputPhone3, 4);
+    const [confirmPassword, setConfirmPassword] = useState()
+    const handleChangeConfirmPassword = (e) => {
+        setConfirmPassword(e.target.value);
+    };
+
+    const submitBtnRef = useRef(null);
+
+
+    useDebounce([password, confirmPassword],
+        () => {
+
+            if (!password) {
+                return;
+            }
+
+            if (!confirmPassword) {
+                return;
+            }
+
+            if (password !== confirmPassword) {
+                submitBtnRef.current.disabled = true;
+                submitBtnRef.current.classList.add('blind');
+            } else {
+                submitBtnRef.current.disabled = false;
+                submitBtnRef.current.classList.remove('blind');
+            }
+        },
+        500);
+
+
+    const inputEmailRef = useRef(null);
+    const inputPasswordRef = useRef(null)
+    const inputConfirmPasswordRef = useRef(null)
+    const inputNicknameRef = useRef(null);
+    const inputNameRef = useRef(null);
+    const inputPhoneRef1 = useRef(null);
+    const inputPhoneRef2 = useRef(null);
+    const inputPhoneRef3 = useRef(null);
+
+    useInputLengthSlicer(inputPhoneRef1, 3);
+    useInputLengthSlicer(inputPhoneRef2, 4);
+    useInputLengthSlicer(inputPhoneRef3, 4);
 
     const handleSignUp = () => {
-        const email = inputEmail.current.value;
-        const password = inputPassword.current.value;
-        const confirmPassword = inputConfirmPassword.current.value;
-        const nickname = inputNickname.current.value;
-        const name = inputName.current.value;
-        const phone = `${inputPhone1.current.value}${inputPhone2.current.value}${inputPhone3.current.value}`;
+        const email = inputEmailRef.current.value;
+        const password = inputPasswordRef.current.value;
+        const confirmPassword = inputConfirmPasswordRef.current.value;
+        const nickname = inputNicknameRef.current.value;
+        const name = inputNameRef.current.value;
+        const phone = `${inputPhoneRef1.current.value}${inputPhoneRef2.current.value}${inputPhoneRef3.current.value}`;
 
         if (password !== confirmPassword) {
             window.alert("다름")
@@ -67,36 +89,51 @@ function SignUp() {
         <form>
             <div>
                 <span>email : </span>
-                <input type={"email"} name={"email"} placeholder={"email"} ref={inputEmail}/>
+                <input type={"email"} name={"email"} placeholder={"email"} ref={inputEmailRef}/>
             </div>
             <div>
                 <span>password : </span>
-                <input type={"password"} name={"password"} placeholder={"password"} ref={inputPassword}
-                       autoComplete={"off"}/>
+                <input type={"password"} name={"password"} placeholder={"password"}
+                       autoComplete={"off"}
+                       ref={inputPasswordRef}
+                       onChange={handleChangePassword}
+                />
             </div>
             <div>
                 <span>password confirm : </span>
-                <input type={"password"} name={"confirmPassword"} placeholder={"password 확인"} ref={inputConfirmPassword}
-                       autoComplete={"off"}/>
+                <input type={"password"} name={"confirmPassword"} placeholder={"password 확인"}
+                       autoComplete={"off"}
+                       ref={inputConfirmPasswordRef}
+                       onChange={handleChangeConfirmPassword}
+                />
             </div>
             <div>
                 <span>name : </span>
-                <input type={"text"} name={"name"} placeholder={"name"} ref={inputName}/>
+                <input type={"text"} name={"name"} placeholder={"name"} ref={inputNameRef}/>
             </div>
             <div>
                 <span>nickname : </span>
-                <input type={"text"} name={"nickname"} placeholder={"nickname"} ref={inputNickname}/>
+                <input type={"text"} name={"nickname"} placeholder={"nickname"} ref={inputNicknameRef}/>
             </div>
             <div>
                 <span>phone : </span>
-                <input className={"input-phone"} type={"number"} name={"phone1"} placeholder={"010"} ref={inputPhone1}/>
+                <input className={"input-phone"} type={"number"} name={"phone1"} placeholder={"010"}
+                       ref={inputPhoneRef1}
+                />
                 <span> - </span>
-                <input className={"input-phone"} type={"number"} name={"phone2"} placeholder={""} ref={inputPhone2}/>
+                <input className={"input-phone"} type={"number"} name={"phone2"} placeholder={""}
+                       ref={inputPhoneRef2}
+                />
                 <span> - </span>
-                <input className={"input-phone"} type={"number"} name={"phone3"} placeholder={""} ref={inputPhone3}/>
+                <input className={"input-phone"} type={"number"} name={"phone3"} placeholder={""}
+                       ref={inputPhoneRef3}
+                />
             </div>
         </form>
-        <button type={"button"} onClick={handleSignUp}>회원가입</button>
+        <button className={'blind'} type={"button"} disabled={true}
+                ref={submitBtnRef}
+                onClick={handleSignUp}
+        >회원가입</button>
     </>;
 }
 
--- study-react/src/util.js
+++ study-react/src/util.js
@@ -1,4 +1,6 @@
-export const setLocalStorage = (key, value, expirationDate) =>{
+import {useEffect} from "react";
+
+export const setLocalStorage = (key, value, expirationDate) => {
     let item = {
         value: value,
         expirationDate: expirationDate,
@@ -6,7 +8,7 @@ export const setLocalStorage = (key, value, expirationDate) =>{
     localStorage.setItem(key, JSON.stringify(item));
 }
 
-export const getLocalStorage = (key) =>{
+export const getLocalStorage = (key) => {
     const item = JSON.parse(localStorage.getItem(key));
     if (item === null) {
         return null;
@@ -18,4 +20,35 @@ export const getLocalStorage = (key) =>{
         return null;
     }
     return item.value;
+}
+
+export const useInputLengthSlicer = (inputRef, maxLength) => {
+    useEffect(() => {
+        if (!inputRef?.current || typeof maxLength !== 'number') {
+            return;
+        }
+
+        const input = inputRef.current;
+        const limitInputLength = ({target}) => {
+            const currentValue = target.value;
+
+            if (currentValue.length > maxLength) {
+                target.value = currentValue.slice(0, maxLength);
+            }
+        };
+        input.addEventListener('input', limitInputLength);
+        return () => {
+            input.removeEventListener('input', limitInputLength);
+        };
+    }, [inputRef, maxLength]);
+}
+
+
+export function useDebounce(detectTarget, doAction, waitTime) {
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            doAction();
+        }, waitTime);
+        return () => clearTimeout(timeout);
+    }, [detectTarget]);
 }
