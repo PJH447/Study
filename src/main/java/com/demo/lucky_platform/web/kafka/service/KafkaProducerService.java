47d86b4d022a5ceb7ed34f2e67f7bfc46d600905
PJH447 <bopi44777@gmail.com>
2025-06-10 13:26:57 +0900

    kafka callback 방식 추가

--- src/main/java/com/demo/lucky_platform/web/kafka/service/KafkaProducerService.java
+++ src/main/java/com/demo/lucky_platform/web/kafka/service/KafkaProducerService.java
@@ -2,7 +2,10 @@ package com.demo.lucky_platform.web.kafka.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @RequiredArgsConstructor
@@ -12,7 +15,15 @@ public class KafkaProducerService {
 
     public void sendMessage(String message) {
         System.out.println("send message : " + message);
-        this.kafkaTemplate.send("my-topic", message);
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send("my-topic", message);
+
+        future.whenComplete((result, ex) -> {
+            if (ex != null) {
+                System.err.println("메시지 전송 실패: " + ex.getMessage());
+            } else {
+                System.out.println("메시지 전송 성공: " + result.getRecordMetadata());
+            }
+        });
     }
 
 }
